8f4c0e8aa2404bdf6d4e6b9c644befa4651901e1
Marcelo Torres <joaquin_marcelo@hotmail.com>
2023-07-23 12:39:23 -0500

    eureka

---
+++ eureka-service/src/test/java/com/servicio/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- pedido-service/src/main/java/com/servicio/pedido/PedidoServiceApplication.java
+++ pedido-service/src/main/java/com/servicio/pedido/PedidoServiceApplication.java
@@ -2,8 +2,10 @@ package com.servicio.pedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PedidoServiceApplication {
 
 	public static void main(String[] args) {
